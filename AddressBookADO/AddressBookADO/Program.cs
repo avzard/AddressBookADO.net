ce997ef32994a9148f9c4b59fc266f3241ec7530
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-07-23 14:51:34 +0530

    [Abhishek]Create Databse

---
+++ AddressBookADO/AddressBookADO/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AddressBookADO.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook ADO.NET!");
+            Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase");
+            int op = Convert.ToInt16(Console.ReadLine());
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+
+            switch (op)
+            {
+                case 1:
+                    addressBookRepo.Create_Database();
+                    break;
+
+            }
+        }
+    }
+}
