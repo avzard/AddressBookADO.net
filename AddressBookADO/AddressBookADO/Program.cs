302cc7cb31673ecfa2bcde9464b95a0f68a6c2d2
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-07-23 15:57:04 +0530

    [Abhishek]Update Existing Data

--- AddressBookADO/AddressBookADO/Program.cs
+++ AddressBookADO/AddressBookADO/Program.cs
@@ -11,9 +11,11 @@ namespace AddressBookADO.net
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook ADO.NET!");
-            Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase\n2)Create AddressBookTable");
+            Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase\n2)Create AddressBookTable\n3)Inserting Details to DataBasen" +
+                "\n4)retrieve Details From dataBase");
             int op = Convert.ToInt16(Console.ReadLine());
             AddressBookRepo addressBookRepo = new AddressBookRepo();
+            AddressBookModel addressbook = new AddressBookModel();
 
             switch (op)
             {
@@ -22,6 +24,21 @@ namespace AddressBookADO.net
                     break;
                 case 2:
                     addressBookRepo.CreateTables();
+                    break;
+                case 3:
+                    addressbook.FirstName = "Shivaraj";
+                    addressbook.LastName = "Gowda";
+                    addressbook.Address = "Basaveshwar Nagar";
+                    addressbook.City = "Bangalore";
+                    addressbook.State = "Karnataka";
+                    addressbook.Zip = "560079";
+                    addressbook.PhoneNumber = "8618199776";
+                    addressbook.Email = "4shivaraj@gmail.com";
+                    addressBookRepo.AddContact(addressbook);
+                    Console.WriteLine("Record Inserted successfully");
+                    break;
+                case 4:
+                    addressBookRepo.GetAllContact();
                     break;
 
             }
